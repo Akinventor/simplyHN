a235f31d2e4e1fe546079c671f2bc9b1d2770fe2
Adam <22334119+Akinventor@users.noreply.github.com>
2020-04-20 14:49:32 -0800

    Modified Script and HTML for new story format

--- script.js
+++ script.js
@@ -10,21 +10,33 @@ $(function() {
     });
 });
 
+// function fetchStories(stories) {
+//     stories.forEach(element => {
+//         var storyURL = "https://hacker-news.firebaseio.com/v0/item/" + element + ".json";
+//         console.log(storyURL);
+//         $.ajax({
+//             dataType: "json",
+//             url: storyURL,
+//             success: function(result) {
+//                 renderStory(result, storyURL);
+//             }
+//         });
+//     });
+// }
+
 function fetchStories(stories) {
-    stories.forEach(element => {
-        var storyURL = "https://hacker-news.firebaseio.com/v0/item/" + element + ".json";
-        console.log(storyURL);
-        $.ajax({
-            dataType: "json",
-            url: storyURL,
-            success: function(result) {
-                renderStory(result);
-            }
-        });
+    var storyURL = "https://hacker-news.firebaseio.com/v0/item/" + stories[0] + ".json";
+    console.log(storyURL);
+    $.ajax({
+        dataType: "json",
+        url: storyURL,
+        success: function(result) {
+            renderStory(result);
+        }
     });
 }
 
 function renderStory(storyData) {
-    var story = "<h3><a href=\""+ storyData.url + "\">" + storyData.title + "</a></h3>";
-    $("#stories").append(story);
+    var story = "<div id=\"story_container\"><div class=\"story_data\"><div class=\"story_title\"><a class=\"story_link\" href=\"" + storyData.url + "\"><span>" + storyData.title + "</span></a><a class=\"story_discussion\" href=\"https://news.ycombinator.com/item?id=" + storyData.id + "\"><span>(Discussion)</span></a></div><div class=\"story_meta\"><span>" + storyData.score + "</span><span class=\"meta_seperator\">|</span><span><a href=\"https://news.ycombinator.com/user?id=" + storyData.by+ "\"><span>" + storyData.by + "</span></a></span><span class=\"meta_seperator\">|</span><span>" + storyData.time + "</span></div></div></div>"
+    $("#allstories").append(story);
 }
