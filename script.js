777ddc2360e7f9574d086b3aedaa35c8171cb1d5
Adam <22334119+Akinventor@users.noreply.github.com>
2020-04-20 18:25:15 -0800

    Changed a lot, most notably time is human readable

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ $(function() {
 //     });
 // }
 
+// This function is for testing, so I don't make a ton of API calls.
 function fetchStories(stories) {
     var storyURL = "https://hacker-news.firebaseio.com/v0/item/" + stories[0] + ".json";
     console.log(storyURL);
@@ -37,6 +38,36 @@ function fetchStories(stories) {
 }
 
 function renderStory(storyData) {
-    var story = "<div id=\"story_container\"><div class=\"story_data\"><div class=\"story_title\"><a class=\"story_link\" href=\"" + storyData.url + "\"><span>" + storyData.title + "</span></a><a class=\"story_discussion\" href=\"https://news.ycombinator.com/item?id=" + storyData.id + "\"><span>(Discussion)</span></a></div><div class=\"story_meta\"><span>" + storyData.score + "</span><span class=\"meta_seperator\">|</span><span><a href=\"https://news.ycombinator.com/user?id=" + storyData.by+ "\"><span>" + storyData.by + "</span></a></span><span class=\"meta_seperator\">|</span><span>" + storyData.time + "</span></div></div></div>"
+    var story = "<div class=\"story_container\"><div class=\"story_data\"><div class=\"story_title\"><a class=\"story_link\" href=\"" + storyData.url + "\"><span>" + storyData.title + "</span></a> <a class=\"story_discussion\" href=\"https://news.ycombinator.com/item?id=" + storyData.id + "\"><span>(Discussion)</span></a></div><div class=\"story_meta\"><span>Points: " + storyData.score + "</span><span class=\"meta_seperator\"> | </span><span><a href=\"https://news.ycombinator.com/user?id=" + storyData.by+ "\"><span>By " + storyData.by + "</span></a></span><span class=\"meta_seperator\"> | </span><span>" + timeSince(new Date(storyData.time * 1000)) + "</span></div></div></div>"
     $("#allstories").append(story);
 }
+
+// This function, timeSince, is straight up taken from https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
+function timeSince(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+  
+    var interval = Math.floor(seconds / 31536000);
+  
+    if (interval > 1) {
+      return interval + " years ago";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+      return interval + " months ago";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+      return interval + " days ago";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+      return interval + " hours ago";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+      return interval + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+  }
+  var aDay = 24*60*60*1000;
